bb137e824609884c166efbd7c8a7d31dafffc094
hitt bahal <hittbahal2003@gmail.com>
2026-02-09 18:32:41 +0530

    architecture

---
+++ src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DatabaseConfig {
+  constructor(private configService: ConfigService) {}
+
+  get databaseUrl(): string {
+    return this.configService.get<string>('DATABASE_URL', 'postgresql://user:password@localhost:5432/energy_db');
+  }
+
+  get databaseType(): string {
+    return this.configService.get<string>('DATABASE_TYPE', 'postgresql');
+  }
+}
